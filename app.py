7b3cf6e34867e6beabe98e8ddae817d708bfe32a
YonLiud <ejliud@gmail.com>
2022-10-10 22:31:12 +0300

    feat: Draw Box

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 import cv2
-
+import imutils
+import numpy as np
 cap = cv2.VideoCapture(0)
 
 while True:
-    ret, frame = cap.read()
-    cv2.imshow('frame', frame)
+    _, frame = cap.read()
+    cv2.rectangle(frame, (100, 100), (300, 300), (0, 255, 0), 0)
+    
+    cv2.imshow("Frame", frame)
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+    
